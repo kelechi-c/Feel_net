ddfd38e7c34e94703931a2e59df676a465b124c0
Tensor <narutokelechi@gmail.com>
2024-05-01 22:02:08 +0100

    Installed packages

---
+++ audio/audio_data.py
@@ -0,0 +1,7 @@
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import torch.nn.functional as F
+import torchvision
+import torchaudio
+from torch.utils.data import DataLoader, Dataset, ImageFolder
