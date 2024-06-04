33622bfc65de2239ff62a376fae87ae4fd52fc91
Tensor <narutokelechi@gmail.com>
2024-06-04 09:43:26 +0100

    Confiig changes

--- config.py
+++ config.py
@@ -2,11 +2,15 @@ class Configs:
     # General
     epochs = 30
     lr = 0.01
+    batch_size = 32
     
-    # Audio
+
+class audio_config:
+    audio_folder = '/kaggle/input/audio-sentiment-analysis/Audio_Dataset'
+    audio_feature_csv = 'audio_emotions.csv'
     sample_rate = 22000
-    cmap = 'viridis'
+    cmap = 'inferno'
     
-    # Image
+    
+class image_config:
     image_size = 200
-    
