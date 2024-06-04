c4ca3948167a08862dd662886922192f57bcb46c
Tensor <narutokelechi@gmail.com>
2024-06-04 15:37:30 +0100

    Data splitting, config additions, switched to tensorflow

--- audio/audio_data2.py
+++ audio/audio_data2.py
@@ -1,13 +1,16 @@
-import librosa
 import numpy as np
 from sklearn.preprocessing import OneHotEncoder
+from sklearn.model_selection import train_test_split
 from util_funcs import get_audio_files_and_labels, extract_audio_features, audio_file_csv
 from config import audio_config
 
 emotion_dict = {
     'hap': 'happy',
     'dis': 'disgusted',
-    'ang': 'angry'
+    'ang': 'angry',
+    'fea': 'fear',
+    'neu': 'neutral',
+    'sad': 'sad'
 }
 
 # For Audio paths: GEt file paths, labels, then map the extract audio function
@@ -28,7 +31,9 @@ print(x_audio.shape)
 
 # For the labels: replace with complete text, and encode as numbers
 encoder = OneHotEncoder()
+audio_df["emotion_labels"] = audio_df["emotion_labels"].map(emotion_dict)
 
 y_labels = encoder.fit_transform(audio_df[["emotion_labels"]])
 y_labels = np.array(y_labels)
 
+x_audio_train, x_audio_test, y_labels_train, y_labels_test = train_test_split(x_audio, y_labels, test_size=0.15, random_state=123)
--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 class Configs:
     # General
-    epochs = 30
-    lr = 0.01
+    epochs = 35
+    lr = 0.001
     batch_size = 32
     
 
@@ -10,7 +10,5 @@ class audio_config:
     audio_feature_csv = 'audio_emotions.csv'
     sample_rate = 22000
     cmap = 'inferno'
+    num_classes = 6
     
-    
-class image_config:
-    image_size = 200
